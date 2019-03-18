8a0b3064c2a940358815fc88508c2a6d006ef636
erica-y-chen <eyufanchen@gmail.com>
2019-03-18 15:30:16 -0700

    completed mvp

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -1,2 +1,17 @@
 import React from 'react';
 import './Card.css';
+
+import Header from '../HeaderComponents/HeaderContainer';
+import Content from './CardContent';
+
+function Card() {
+    return (
+        <div class ="card"> 
+            <Header />
+            <Content />
+        </div>
+    );
+}
+
+
+export default Card;
--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContent.js
@@ -1,2 +1,16 @@
 import React from 'react';
 import './Card.css';
+
+import Banner from './CardBanner'
+import Article from './CardArticle'
+
+function CardContent() {
+    return(
+        <div class="content">
+            <Banner />
+            <Article />
+        </div>
+    )
+}
+
+export default CardContent;
