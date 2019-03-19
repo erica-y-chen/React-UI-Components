7acf8e6d23c4c9d5d2eb0f9c80473db2d9fe97ec
erica-y-chen <eyufanchen@gmail.com>
2019-03-19 14:48:44 -0700

    completed mvp

--- Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
+++ Project-II-Calculator/calculator/src/components/DisplayComponents/CalculatorDisplay.js
@@ -1,2 +1,14 @@
 import React from 'react';
 import './Display.css';
+
+
+function Screen() {
+    return (
+        <div className="total">
+            <h1>0</h1>
+        </div>
+    )
+}
+
+
+export default Screen;
